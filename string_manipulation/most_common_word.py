e85ac766f75dd6ee381c236f97397ae0f4feaf0e
hyunhoon-j <59475735+hyunhoon-j@users.noreply.github.com>
2021-04-23 00:06:30 +0900

    Add files via upload

---
+++ string_manipulation/most_common_word.py
@@ -0,0 +1,37 @@
+def mostCommonWord(paragraph, banned):
+    p = paragraph.lower()
+
+    # Replace any sentence end mark into ''
+    mark = ['.', ',', '?', '!', ';', ':', "'"]
+    for i in mark:
+        p = p.replace(i, ' ')
+
+    # Split the string
+    p = p.split()
+
+    print(p)
+
+    if len(banned) != 0:
+        for i in banned:
+            print(i)
+            while i in p:
+                p.remove(i)
+
+    print(p)
+
+    # Make a dictionary to find the most common word
+    counts = dict()
+    for elem in p:
+        counts[elem] = counts.get(elem, 0) + 1
+
+    bigword = None
+    bigcount = None
+
+    for word, count in counts.items():
+        if bigcount is None or count > bigcount:
+            bigcount = count
+            bigword = word
+
+    return bigword
+
+print(mostCommonWord("a, a, a, a, b,b,b,c, c", ["a"]))
