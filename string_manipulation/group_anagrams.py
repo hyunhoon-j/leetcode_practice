4b5e44c4666f0fd84e61ae7ec5f666a40231bad8
hyunhoon-j <59475735+hyunhoon-j@users.noreply.github.com>
2021-04-28 14:09:36 +0900

    Add files via upload

---
+++ string_manipulation/group_anagrams.py
@@ -0,0 +1,22 @@
+def groupAnagrams(strs):
+    # Sort each word to make a norm
+    word_srt_lst = [str(sorted(i)) for i in strs]
+
+    # Make a set to make a frame of return value: [[], []...]
+    tmp_set = set(word_srt_lst)
+
+    new_lst = list()
+    for i in range(len(tmp_set)):
+        new_inner = list()
+        new_lst.append(new_inner)
+
+    clsf_lst = list(tmp_set)
+
+    for i in range(len(clsf_lst)):
+        for word in strs:
+            if str(sorted(word)) == clsf_lst[i]:
+                new_lst[i].append(word)
+
+    return new_lst
+
+print(groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
