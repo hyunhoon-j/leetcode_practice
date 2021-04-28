3cc1be983e0df43b5dcf1614a1364c114212373d
hyunhoon-j <59475735+hyunhoon-j@users.noreply.github.com>
2021-04-28 23:46:32 +0900

    Add files via upload

---
+++ string_manipulation/group_anagrams_sol.py
@@ -0,0 +1,11 @@
+import collections
+
+def groupAnagrams(strs):
+    anagrams = collections.defaultdict(list)
+
+    for word in strs:
+        anagrams[''.join(sorted(word))].append(word)
+
+    return list(anagrams.values())
+
+print(groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
