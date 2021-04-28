328bb8a10640d184baef449d3be748178a9d663d
hyunhoon-j <59475735+hyunhoon-j@users.noreply.github.com>
2021-04-28 14:08:45 +0900

    Update reverse_string.py

--- string_manipulation/reverse_string.py
+++ string_manipulation/reverse_string.py
@@ -1,4 +1,4 @@
-# leetcode do not want to return the value from what I compute in the function.
+# leetcode does not want to return the value from what I compute in the function.
 
 def reverseString(s):
     s.reverse()
