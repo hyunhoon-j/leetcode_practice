640fab19ba0a7bd2c425b4fe2699b4a9f42ee991
hyunhoon-j <59475735+hyunhoon-j@users.noreply.github.com>
2021-05-12 12:06:59 +0900

    Update two_sum.py

--- array/two_sum.py
+++ array/two_sum.py
@@ -1,13 +1,3 @@
-nums = [2, 7, 11, 15, 19]
-target = 9
-
-# A solution by brute force algorithm
-
-for i in range(len(nums)):
-    for j in range(i+1, len(nums)):
-        if nums[i] + nums[j] == target:
-            print(i, j)
-
 def twoSum(nums, target):
     for i in range(len(nums)):
         for j in range(i+1, len(nums)):
