4131c22f56b03689caf4ac64c137e9799c875568
hyunhoon-j <59475735+hyunhoon-j@users.noreply.github.com>
2021-05-13 11:25:07 +0900

    Add files via upload

---
+++ array/three_sum_twopointer.py
@@ -0,0 +1,41 @@
+# nums = [-1,0,1,2,-1,-4]
+
+def threeSum(nums):
+    results = list()
+    # Sort numbers in a list
+    nums.sort()
+
+    for i in range(len(nums) - 2):
+        # Skip the duplicated number
+        if i > 0 and nums[i] == nums[i-1]:
+            continue
+
+        # Two pointer
+        # Make indices
+        left = i + 1
+        right = len(nums) - 1
+
+        # Control the movement of each pointer
+        while left < right:
+            # Check sum
+            sum = nums[i] + nums[left] + nums[right]
+            if sum < 0:
+                left = left + 1
+            elif sum > 0:
+                right = right - 1
+            else:
+                results.append([nums[i], nums[left], nums[right]])
+
+                # Skip the duplicated number because the number is already used.
+                while left < right and nums[left] == nums[left+1]:
+                    left = left + 1
+                while left < right and nums[right] == nums[right-1]:
+                    right = right - 1
+
+                # Set the next indices
+                left = left + 1
+                right = right - 1
+
+    return results
+
+print(threeSum([-1,0,1,2,-1,-4]))
