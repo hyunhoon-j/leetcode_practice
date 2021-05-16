a8c31ff83da649454e1762c41c0def1a6e3caeb6
hyunhoon-j <59475735+hyunhoon-j@users.noreply.github.com>
2021-05-16 17:33:57 +0900

    Add files via upload

---
+++ array/array_sol.py
@@ -0,0 +1,17 @@
+# nums = [6,2,6,5,1,2]
+
+def arrayPairSum(nums):
+    sum = 0
+    pair = list() # We are going to focus only one pair composed by the numbers from sorted order of the list
+    nums.sort()
+
+    for n in nums:
+        pair.append(n)
+        if len(pair) == 2: # Focus on just two numbers because we can get the max value from the min value of that numbers
+            sum = sum + min(pair)
+            pair = list()
+
+    return sum
+
+print(arrayPairSum([6,2,6,5,1,2]))
+# Answer: 9
