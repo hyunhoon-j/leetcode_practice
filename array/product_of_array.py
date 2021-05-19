d645f724d866c82035033377b42445a7f077607a
hyunhoon-j <59475735+hyunhoon-j@users.noreply.github.com>
2021-05-19 14:56:20 +0900

    Add files via upload

---
+++ array/product_of_array.py
@@ -0,0 +1,19 @@
+nums = [-1,1,0,-3,3]
+nums = tuple(nums)
+
+fin_lst = list()
+
+for i in range(len(nums)):
+    nums_tmp = list(nums)
+    nums_tmp.remove(nums[i])
+
+    tmp_str = None
+    for num in nums_tmp:
+        if tmp_str == None:
+            tmp_str = num
+        else:
+            tmp_str = tmp_str * num
+
+    fin_lst.append(tmp_str)
+
+print(fin_lst)
