5ddad9cc21315b2f549014f92e6277df7629498a
hyunhoon-j <59475735+hyunhoon-j@users.noreply.github.com>
2021-05-22 15:43:07 +0900

    Update product_of_array.py

--- array/product_of_array.py
+++ array/product_of_array.py
@@ -1,21 +1,23 @@
 # Timeout...
 
-nums = [-1,1,0,-3,3]
-nums = tuple(nums)
+def productExceptSelf(nums):
+    nums = tuple(nums)
 
-fin_lst = list()
+    fin_lst = list()
 
-for i in range(len(nums)):
-    nums_tmp = list(nums)
-    nums_tmp.remove(nums[i])
+    for i in range(len(nums)):
+        nums_tmp = list(nums)
+        nums_tmp.remove(nums[i])
 
-    tmp_str = None
-    for num in nums_tmp:
-        if tmp_str == None:
-            tmp_str = num
-        else:
-            tmp_str = tmp_str * num
+        tmp_str = None
+        for num in nums_tmp:
+            if tmp_str == None:
+                tmp_str = num
+            else:
+                tmp_str = tmp_str * num
 
-    fin_lst.append(tmp_str)
+        fin_lst.append(tmp_str)
 
-print(fin_lst)
+    return fin_lst
+
+print(productExceptSelf([-1,1,0,-3,3]))
