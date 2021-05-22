1a11aacd5cd76d38d262c5cdbd5b96dafc57deb6
hyunhoon-j <59475735+hyunhoon-j@users.noreply.github.com>
2021-05-22 15:31:50 +0900

    Update max_profit_sol.py

--- array/max_profit_sol.py
+++ array/max_profit_sol.py
@@ -3,7 +3,7 @@ import sys
 def maxProfit(prices):
     # prices = [7, 1, 5, 3, 6, 4]
     profit = 0
-    min_price = sys.maxsize # We need to set the min_price as high as possible to change the value to what we found from iteration
+    min_price = sys.maxsize # We need to set the min_price as high as possible to change this value to what we found from iteration
 
     for price in prices:
         min_price = min(min_price, price)
