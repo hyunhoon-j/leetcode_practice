ff62a723d8b9022c77fcf5faac93fd49688166e5
hyunhoon-j <59475735+hyunhoon-j@users.noreply.github.com>
2021-05-29 15:37:06 +0900

    Update array_sol.py

--- array/array_sol.py
+++ array/array_sol.py
@@ -2,7 +2,7 @@
 
 def arrayPairSum(nums):
     sum = 0
-    pair = list() # We are going to focus on only one pair composed by the numbers from sorted order of the list
+    pair = list() # We are going to focus on only one pair composed of the numbers from the sorted list
     nums.sort() # [1, 2, 2, 5, 6, 6]
 
     for n in nums:
